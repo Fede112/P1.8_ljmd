98029cdfc1034d18ac1acc2424c88cf22f63a96c
asaleh33 <salehastro@gmail.com>
2019-01-28 14:04:00 +0100

    setting ctypes structure

--- python-interface/ljmd.py
+++ python-interface/ljmd.py
@@ -14,4 +14,29 @@ print kboltz, mvsq2e
 result = kboltz + mvsq2e
 print result
 
+# ctypes structures:
 
+class mdsys_t(Structure):
+  _fields_ = [
+    ('natoms', c_int),
+    ('nfi', c_int), 
+    ('nsteps', c_int),
+    ('dt', c_double),
+    ('mass', c_double),
+    ('epsilon', c_double),
+    ('sigma', c_double),
+    ('box', c_double),
+    ('rcut', c_double),
+    ('ekin', c_double),
+    ('epot', c_double),
+    ('temp', c_double),
+    ('rx', POINTER(c_double)), 
+    ('ry', POINTER(c_double)), 
+    ('rz', POINTER(c_double)), 
+    ('vx', POINTER(c_double)), 
+    ('vy', POINTER(c_double)),  
+    ('vz', POINTER(c_double)), 
+    ('fx', POINTER(c_double)), 
+    ('fy', POINTER(c_double)), 
+    ('fz', POINTER(c_double)),      
+  ]  
